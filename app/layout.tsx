bd30fef82ba63319f2c1e7b41fc6d6c4a18b65ed
aricionur <aricionur@gmail.com>
2023-06-16 23:26:27 +0300

    Core modal and button components were created

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 
 import Navbar from './components/navbar/Navbar'
+import Modal from './components/modals/Modal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,10 +11,15 @@ export const metadata = {
   description: 'Airbnb clone',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Modal isOpen />
         <Navbar />
         {children}
       </body>
