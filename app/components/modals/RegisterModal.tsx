3ada481d7df85db337cd838e0b06cf3f288b6c7a
aricionur <aricionur@gmail.com>
2023-06-18 18:17:51 +0300

    Prisma models created, connected to mongodb replica, users registered to db, authentication implemented

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,7 +6,7 @@ import { FcGoogle } from 'react-icons/fc'
 import { useCallback, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 
-import useRegisterModal from '@/app/ hooks/useRegisterModal'
+import useRegisterModal from '@/app/hooks/useRegisterModal'
 import Modal from './Modal'
 import Heading from '../core/Heading'
 import Input from '../inputs/Input'
@@ -39,22 +39,8 @@ const RegisterModal = () => {
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome to Airbnb" subtitle="Create an account!" />
-      <Input
-        id="email"
-        label="Email"
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-        required
-      />
-      <Input
-        id="name"
-        label="Name"
-        disabled={isLoading}
-        register={register}
-        errors={errors}
-        required
-      />
+      <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
+      <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
       <Input
         id="password"
         label="Password"
@@ -70,25 +56,12 @@ const RegisterModal = () => {
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
-      <Button
-        outline
-        label="Continue with Google"
-        icon={FcGoogle}
-        onClick={() => {}}
-      />
-      <Button
-        outline
-        label="Continue with Github"
-        icon={AiFillGithub}
-        onClick={() => {}}
-      />
+      <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} />
+      <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => {}} />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
           <div>Already have account?</div>
-          <div
-            onClick={registerModal.onClose}
-            className="text-neutral-800 cursor-pointer hover:underline"
-          >
+          <div onClick={registerModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">
             Log in
           </div>
         </div>
