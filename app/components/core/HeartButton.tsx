9fc8b952bfb13482c2553b146db000585600ebbe
aricionur <aricionur@gmail.com>
2023-06-27 12:15:49 +0300

    Apiroute added to save favorites of an user

--- app/components/core/HeartButton.tsx
+++ app/components/core/HeartButton.tsx
@@ -3,17 +3,18 @@
 import { User } from '@prisma/client'
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 
+import useFavorite from '@/app/hooks/useFavorite'
+
 interface HeartButtonProps {
   listingId: string
-  currentUser?: User
+  currentUser: User | null
 }
 
 const HeartButton: React.FC<HeartButtonProps> = ({ currentUser, listingId }) => {
-  const hasFavorited = true
-  const toogleFavorite = () => {}
+  const { hasFavorited, toggleFavorite } = useFavorite({ currentUser, listingId })
 
   return (
-    <div onClick={toogleFavorite} className="relative hover:opacity-80 transition cursor-pointer">
+    <div onClick={toggleFavorite} className="relative hover:opacity-80 transition cursor-pointer">
       <AiOutlineHeart size={28} className="fill-white absolute -top-[2px] -right-[2px]" />
       <AiFillHeart size={24} className={`${hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'}`} />
     </div>
