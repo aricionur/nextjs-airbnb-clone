01df16325da140731e832084a0e00da50e12327e
aricionur <aricionur@gmail.com>
2023-07-05 17:30:10 +0300

    Search modal added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import ToasterProvider from './providers/ToasterProvider'
 import LoginModal from './components/modals/LoginModal'
 import getCurrentUser from './actions/getCurrentUser'
 import RentModal from './components/modals/RentModal'
+import SearchModal from './components/modals/SearchModal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
     <html lang="en">
       <body className={inter.className}>
         <ToasterProvider />
+        <SearchModal />
         <RegisterModal />
         <LoginModal />
         <RentModal />
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Container from './components/core/Container'
 import EmptyState from './components/core/EmptyState'
 import ListingCard from './components/listings/ListingCard'
 
+export const dynamic = 'force-dynamic'
+
 interface HomeProps {
   searchParams: IListingsParams
 }
